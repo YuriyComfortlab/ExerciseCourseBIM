b4329749e20350a4c94fa04308ab0ba21209395a
Юрий Дембицкий <y.d.asutp@gmail.com>
2021-10-26 11:14:09 +0700

    json ready

---
+++ Exercise16JSON/JsonFromFile/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Необходимо разработать программу для получения информации о товаре из json-файла.
+Десериализовать файл «Products.json» из задачи 
+1. Определить название самого дорогого товара.
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using JsonToFile;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace JsonFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "d:/BIM/Products.json";
+
+            string jsonString = File.ReadAllText(path);
+            Console.WriteLine(jsonString);
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(jsonString);
+            double max = 0;
+            string maxPrice ="";
+            foreach (var product in products)
+            {
+                if (max< product.PriceProduct)
+                {
+                    max = product.PriceProduct;
+                    maxPrice = product.NameProduct;
+                }       
+            }
+            Console.WriteLine("Максимальная цена у товара \"{0}\" = {1:F2} руб. ", maxPrice, max);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exercise16JSON/JsonToFile/Program.cs
@@ -0,0 +1,72 @@
+﻿/*1.    Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json.
+
+Разработать класс для моделирования объекта «Товар». 
+Предусмотреть члены класса «Код товара» (целое число), «Название товара» (строка), «Цена товара» (вещественное число).
+Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JsonToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "d:/BIM/Products.json";
+            int number = 5;
+            Product[] products = new Product[number];
+            int code = 0;
+            string name = "";
+            double price = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Введите данные товара {0}", i + 1);
+                Console.Write("Введите код продукта ");
+                code = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите название продукта ");
+                name = Console.ReadLine();
+
+                Console.Write("Введите цену продукта ");
+                price = Convert.ToDouble(Console.ReadLine());
+
+                products[i] = new Product()
+                { CodeProduct = code, NameProduct = name, PriceProduct = price };
+                Console.WriteLine();
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(products, options);
+            File.WriteAllText(path, jsonString);          
+           
+        }
+
+    }
+    public class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int CodeProduct { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string NameProduct { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double PriceProduct { get; set; }
+    }
+
+
+}
