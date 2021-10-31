6c675dee9d22f1dfe9cbb50dd8d81f4b936d369d
Юрий Дембицкий <y.d.asutp@gmail.com>
2021-10-31 21:47:35 +0700

    Factorial ready

---
+++ Exercise23AsyncPrg/FactorialAsync/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Разработать асинхронный метод для вычисления факториала числа. 
+ * В методе Main выполнить проверку работы метода.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Numerics;
+
+namespace FactorialAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, факториал которого нужно найти ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            BigInteger result = FactorialAsync(number).Result;
+            Console.WriteLine("Факториал числа {0:0,0}", result);    
+
+            Console.ReadKey();
+        }
+        static BigInteger Factorial(int number) //метод возаращает значение
+        {           
+            BigInteger fact = 1;
+            Console.WriteLine("Идет расчет...");
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= (BigInteger)i;
+                Thread.Sleep(500);
+            }          
+            return (fact);
+        }
+        static async Task<BigInteger> FactorialAsync(int number)
+        {
+            Console.WriteLine(" Расчет запущен: ");
+            BigInteger result = await Task.Run(() => Factorial(number)); 
+            Console.WriteLine(" Расчет закончен");
+            return result;
+        }
+    }
+}
---
+++ Exercise23AsyncPrg/FactorialAsyncWithoutResult/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Разработать асинхронный метод для вычисления факториала числа. 
+ * В методе Main выполнить проверку работы метода.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Numerics;
+
+namespace FactorialAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, факториал которого нужно найти ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            FactorialAsync(number);
+            Console.WriteLine("А пока выполняем другие методы");
+
+            Console.ReadKey();
+        }
+        static void Factorial(int number) //метод не возвращает значение
+        {
+            BigInteger fact = 1;
+            Console.WriteLine("Идет расчет факториала...");
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= (BigInteger)i;
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("Факториал числа {0:0,0}", fact);
+        }
+        static async Task FactorialAsync(int number)
+        {
+            Console.WriteLine(" Расчет запущен: ");
+            await Task.Run(() => Factorial(number));
+            Console.WriteLine(" Расчет закончен");
+        }
+    }
+}
